4195ea803ca087fb90e030f2e697c7d77a93a0cc
Guilherme Luís <guilherme_silva_luis@hotmail.com>
2026-03-31 20:00:07 -0300

    Move DB_URL check into connectDB function
    correct some issues about db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,14 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-const { DB_URL } = process.env;
-
-if (!DB_URL) {
-    throw new Error("DB_URL is not defined in environment variables");
-}
-
 export const prisma = new PrismaClient();
 
 export async function connectDB() {
+    const { DB_URL } = process.env;
+    
+    if (!DB_URL) {
+        throw new Error("DB_URL is not defined in environment variables");
+    }
     try{
         await prisma.$connect();
         //console.log(`Prisma connected to MongoDB successfully`);
